19bc5c2f93a26287a35f9c36dcda03a6c28e5da5
Oleg Koltunov <koltunov.o@gmail.com>
2017-07-19 19:23:05 +0300

    third commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Task-React-App
+https://koltunovoleg.github.io/Task-React-App/
+
+
+1. git clone https://github.com/KoltunovOleg/Task-React-App.git
+2. npm install
+3. npm start 
+4. go to http://localhost:8080
+
+To build bundle.js:
+1. npm run build
+2. look the folder 'public/'
--- app/Registration.js
+++ app/Registration.js
@@ -2,27 +2,30 @@ import React, { Component } from 'react';
 
 //It is compare pass & pass_confirm
 function toCompare(...args) {
-
-  let retrievedObject = localStorage.getItem(args[0]);
-  if (retrievedObject) {
-    alert('Such login already exists. Try again please.');
+  console.log(`toCompare: ${args}`);
+  if (!args[0] || !args[1] || !args[2]) {
+    alert('Try again please.');
     return false;
   } else {
-    if (args[1] === args[2]) {
-      saveDataUser(args[0], args[1]);
-      return true;
-    } else {
-      alert('Passwords do not match. Try again please.');
+    let retrievedObject = localStorage.getItem(args[0]);
+    if (retrievedObject) {
+      alert('Such login already exists. Try again please.');
       return false;
+    } else {
+      if (args[1] === args[2]) {
+        saveDataUser(args[0], args[1]);
+        return true;
+      } else {
+        alert('Passwords do not match. Try again please.');
+        return false;
+      }
     }
   }
-  console.log(`toCompare: ${args}`)
-
 }
 
 //It is save email & pass to localstorage
-function saveDataUser(email, pass){
-  let arrDataUser = {pass: pass};
+function saveDataUser(email, pass) {
+  let arrDataUser = { pass: pass };
   console.log(arrDataUser);
   localStorage.setItem(email, JSON.stringify(arrDataUser));
 }
@@ -32,30 +35,30 @@ function saveDataUser(email, pass){
 class Registration extends Component {
 
   handleSubmit(event) {
-    let email= event.target.email.value,
-        pass = event.target.pass.value,
-        pass_confirm = event.target.pass_confirm.value,
-        getdata = toCompare(email, pass, pass_confirm);
-        this.props.onchangeAccess(getdata);
+    let email = event.target.email.value,
+      pass = event.target.pass.value,
+      pass_confirm = event.target.pass_confirm.value,
+      getdata = toCompare(email, pass, pass_confirm);
+    this.props.onchangeAccess(getdata);
 
-        event.preventDefault();
-    }
+    event.preventDefault();
+  }
 
-  render(){
+  render() {
     return (
       <div>
         <h3 className="h3">Registartion</h3>
         <form onSubmit={this.handleSubmit.bind(this)}>
-                <div className="form-group">
-                  <input className="form-control" name="email" type="mail" placeholder="Email" />
-                </div>
-                <div className="form-group">
-                  <input className="form-control" name="pass" type="password" placeholder="Password" />
-                </div>
-                <div className="form-group">
-                  <input className="form-control" name="pass_confirm" type="password" placeholder="Confirm Password" />
-                </div>
-                <button className="btn btn-default" type="submit">Registration</button>
+          <div className="form-group">
+            <input className="form-control" name="email" type="mail" placeholder="Email" />
+          </div>
+          <div className="form-group">
+            <input className="form-control" name="pass" type="password" placeholder="Password" />
+          </div>
+          <div className="form-group">
+            <input className="form-control" name="pass_confirm" type="password" placeholder="Confirm Password" />
+          </div>
+          <button className="btn btn-default" type="submit">Registration</button>
         </form>
       </div>
     )
